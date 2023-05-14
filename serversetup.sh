c4fcc9d8398ef09632428fcd4221a8b76dd47d4c
Ameenah21 <aminatlawal21@gmail.com>
2023-05-14 14:08:05 +0100

    Initial Files

---
+++ serversetup.sh
@@ -0,0 +1,20 @@
+sudo apt-get update -y
+
+sudo apt-get install python3-distutils
+
+sudo apt-get install python3-apt
+
+sudo apt-get install wget
+
+wget https://bootstrap.pypa.io/get-pip.py
+
+sudo python3 get-pip.py
+
+pip install mage-ai
+
+mage start uber-project
+
+sudo pip3 install google-cloud
+
+
+sudo pip3 install google-cloud-bigquery
